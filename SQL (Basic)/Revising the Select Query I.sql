b46e4128b10f37a6508504dfd8d62611d799f286
Arpit Sarkar <100153057+arpit-sarkar@users.noreply.github.com>
2022-04-11 22:38:19 +0530

    Add files via upload

---
+++ SQL (Basic)/Revising the Select Query I.sql
@@ -0,0 +1,14 @@
+/*
+SKILL: SQL (Basic)
+DIFFICULTY: Easy
+SUBDOMAIN: Basic Select
+
+CHALLENGE: Revising the Select Query I
+
+*********************************************************************************
+*/
+
+
+SELECT *
+FROM CITY
+WHERE POPULATION>100000 AND CountryCode = 'USA'
